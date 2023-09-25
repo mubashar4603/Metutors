13ae1a3b217df9d7aab41566725ba59c5cde3f96
Mubashar <mubashar4603@gmail.com>
2023-09-25 19:01:18 +0500

    pytest html rport setup

--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -1,15 +1,20 @@
 import configparser
+
 config = configparser.RawConfigParser()
 config.read("/home/mubashar4603/PycharmProjects/Metutors/Configurations/config.ini")
+
+
 class ReadConfig():
     @staticmethod
     def getAppURL():
         url = config.get('common info', 'baseURL')
         return url
+
     @staticmethod
     def getUsername():
         username = config.get('common info', 'username')
         return username
+
     @staticmethod
     def getPassword():
         password = config.get('common info', 'password')
